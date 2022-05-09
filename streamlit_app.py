d289316c03b8afa920fb0657f0c99a24c3534e2b
dangarrison <dangarrison@users.noreply.github.com>
2022-05-09 16:35:25 -0700

    Update streamlit_app.py
    Fixed bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Add multiselect box
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avacado', 'Strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 #Display the table on the page
 streamlit.dataframe(my_fruit_list)
